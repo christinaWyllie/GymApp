e492ed8c5d536ae3b094ca5a1121c4506f79e6af
isaacdtc <111832488+isaacdtc@users.noreply.github.com>
2022-11-28 17:08:33 -0700

    Add files via upload

--- MainApp.py
+++ MainApp.py
@@ -9,6 +9,8 @@ from kivy.properties import ObjectProperty
 from kivy.uix.floatlayout import FloatLayout
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.popup import Popup
+from kivy.uix.tabbedpanel import TabbedPanel
 
 class Homepage(Screen):
     pass
@@ -21,30 +23,71 @@ class RegForm(Screen):
     addr = ObjectProperty(None)
     phone = ObjectProperty(None)
     ssn = ObjectProperty(None)
+    passw = ObjectProperty(None)
+
+    error = 0
     pass
 
     def submit(self):
-        print("Name: ", self.fname.text, self.lname.text)
-        print("Email: ", self.email.text)
-        print("Address: ", self.addr.text)
-        print("Phone Number: ", self.phone.text)
-        print("SSN: ", self.ssn.text)
+        if (self.email.text.find("@") != -1 and self.email.text.find(".") != -1):
+            if len(self.phone.text) == 10:
+                if len(self.ssn.text) == 9:
+                    print("Name: ", self.fname.text, self.lname.text)
+                    print("Email: ", self.email.text)
+                    print("Address: ", self.addr.text)
+                    print("Phone Number: ", self.phone.text)
+                    print("SSN: ", self.ssn.text)
+                    print("Password: ",self.passw.text)
+                    error = 0
+                    self.reset()
+                else:
+                    error = 1
+                    invalidReg("ssn")
+            else:
+                error = 1
+                invalidReg("phone")
+        else:
+            error = 1
+            invalidReg("email")
+    
+    def reset(self):
+        self.fname.text = ""
+        self.lname.text = ""
+        self.email.text = ""
+        self.addr.text = ""
+        self.phone.text = ""
+        self.ssn.text = ""
+        self.passw.text = ""
+
+def invalidReg(type):
+    if (type == "email"):
+        pop = Popup(title = "Error while creating account",
+                    content = Label(text="Invalid email."),
+                    size_hint=(None,None), size=(400,400))
+        pop.open()
+    elif (type == "phone"):
+        pop = Popup(title = "Error while creating account",
+                    content = Label(text="Invalid phone number."),
+                    size_hint=(None,None), size=(400,400))
+        pop.open()
+    elif (type == "ssn"):
+        pop = Popup(title = "Error while creating account",
+                    content = Label(text="Invalid SSN."),
+                    size_hint=(None,None), size=(400,400))
+        pop.open()
 
 
 class LoginForm(Screen):
-    # fname = ObjectProperty(None)
-    # lname = ObjectProperty(None)
-    # email = ObjectProperty(None)
-    # addr = ObjectProperty(None)
+    email = ObjectProperty(None)
+    passw = ObjectProperty(None)
     pass
 
-    # def submit(self):
-    #     #Possible SQL query
-    #     print("First Name:", self.fname.text, "Email:", self.email.text)
-    #     self.fname.text = ""
-    #     self.lname.text = ""
-    #     self.email.text = ""
-    #     self.addr.text = ""
+    def submit(self):
+        #Possible SQL query
+        print("Email:", self.email.text)
+        print("Password:", self.passw.text)
+        self.email.text = ""
+        self.passw.text = ""
 
 class PageManager(ScreenManager):
     pass

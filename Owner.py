8425da9fd9ea74a1bf5e1a31cab7855220cc7eb4
christinaWyllie <100972547+christinaWyllie@users.noreply.github.com>
2022-12-03 15:07:42 -0700

    sched

--- Owner.py
+++ Owner.py
@@ -9,8 +9,9 @@ class Owner(Admin):
         self.address = "2500 University Dr NW, Calgary, AB T2N 1N4"
         self.phone = 4034034033
         self.email = "jalal.kawash@ucalgary.ca"
-        createPerson(self.ssn, self.fname, self.lname, self.address, self.phone, self.email)
-        createOwner(self.ssn, self.email, self.phone, self.fname, self.lname, self.address)
+        self.password = "cpsc"
+        createPerson(self.ssn, self.fname, self.lname, self.address, self.phone, self.email, self.password)
+        createOwner(self.ssn, self.email, self.phone, self.fname, self.lname, self.address, self.password)
         self.id = getOwnerID(self.ssn)
         
     def getowner(self):

b26a552a723eeffece7a08a491830eaca51e4838
christinaWyllie <100972547+christinaWyllie@users.noreply.github.com>
2022-12-03 17:46:55 -0700

    h

--- Populate.sql
+++ Populate.sql
@@ -5,7 +5,8 @@ VALUES 	(123456788,"tina", "w", "24 School Drive", "password", 4034034033, "t.w@
         (466709091, "member1", "stark", "87 Member street", "password", 4039925656, "member1@ucalgary.ca"),
         (466707071, "member2", "mcdonald", "91 Member street", "password", 4038855858, "member2@ucalgary.ca"),
         (466606061, "member3", "smith",  "3 Member street", "password", 4039919911, "member3@ucalgary.ca"), 
-        (123333335,"manager", "f", "24 Manager Lane", "password", 4035445454, "manager@ucalgary.ca");
+        (123333335,"manager", "f", "24 Manager Lane", "password", 4035445454, "manager@ucalgary.ca"), 
+        (123366775, "owner", "j","24 Owner Lane", "password",  4035690012, "owner@ucalgary.ca");
 
 INSERT INTO RESTRICTED_USER
 VALUES  (123456788, 1, "t.w@ucalgary.ca", "tina", "w", "24 School Drive", "password", 4034034033), 
@@ -25,10 +26,14 @@ INSERT INTO TRAINER
 VALUES (123456798, 3, "i.d@ucalgary.ca", "isaac", "d", "22 School Drive", 4034034043, "password", 1);
 
 INSERT INTO ADMIN
-VALUES (123333335, 1, "manager@ucalgary.ca", "manager", "f", "24 Manager Lane", "password", 4035445454);
+VALUES (123333335, 1, "manager@ucalgary.ca", "manager", "f", "24 Manager Lane", "password", 4035445454), 
+		(123366775, 2, "owner@ucalgary.ca", "owner", "j","24 Owner Lane", "password",  4035690012);
 
 INSERT INTO MANAGER
 VALUES (123333335, 1, "manager@ucalgary.ca", 1, "manager", "f", "24 Manager Lane", "password", 4035445454);
+
+INSERT INTO OWNER
+VALUES (123366775, 2, "owner@ucalgary.ca", 1, "owner", "j", "password","24 Owner Lane", 4035690012);
 
 
 INSERT INTO CLIENT
@@ -51,4 +56,4 @@ VALUES (1, "Monday" , "1:00"),
         (1, "Tuesday", "1:00"), 
         (2, "Monday", "1:00");
         
-SELECT * FROM ROOMS
+SELECT * FROM OWNER
